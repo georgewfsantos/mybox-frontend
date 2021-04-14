d9463d088d3e9c32d778543cf37a795896734778
George W F Santos <gweverton21@hotmail.com>
2021-04-14 18:59:22 -0300

    Remove ApplicationGlobalContext children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import "./App.css";
 
 import Routes from "./routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Helmet>
+        <meta property="og:title" content="This is my title" />
+        <meta
+          property="og:url"
+          content="https://mybox-frontend.herokuapp.com"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="This is my testing description"
+        />
+        <meta property="og:image" content="./meta_image.jpeg" />
+      </Helmet>
+      <Routes />
+    </>
+  );
 }
 
 export default App;
