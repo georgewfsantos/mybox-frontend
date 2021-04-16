ff374a68040110b02f578c0052bb7d80c98e8bf4
George W F Santos <gweverton21@hotmail.com>
2021-04-16 18:20:05 -0300

    remove testing code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import "./App.css";
 
 import Routes from "./routes";
 
 function App() {
-  return (
-    <>
-      <Helmet>
-        <meta property="og:title" content="This is my title" />
-        <meta
-          property="og:url"
-          content="https://mybox-frontend.herokuapp.com"
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:description"
-          content="This is my testing description"
-        />
-        <meta property="og:image" content="./meta_image.jpeg" />
-        <Routes />
-      </Helmet>
-    </>
-  );
+  return <Routes />;
 }
 
 export default App;
